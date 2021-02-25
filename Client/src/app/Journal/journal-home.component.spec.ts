c9e3af31758f9f2ad8140ca7352ca9686ad95145
gergely.eory <gergely.eory@gmail.com>
2021-02-25 09:35:47 +0000

    initial commit

---
+++ Client/src/app/Journal/journal-home.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JournalHomeComponent } from './journal-home.component';
+
+describe('JournalHomeComponent', () => {
+  let component: JournalHomeComponent;
+  let fixture: ComponentFixture<JournalHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JournalHomeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JournalHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Client/src/app/Journal/journal-home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'journal-home',
+  template: `
+    <p>journal-home works!</p>
+  `,
+  styleUrls: [``]
+})
+export class JournalHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
