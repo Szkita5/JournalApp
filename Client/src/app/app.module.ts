f9941f38aee8e09fdb0afafd90494b756624f1e2
gergely.eory <gergely.eory@gmail.com>
2021-02-25 11:43:56 +0000

    secret key

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { JournalHomeComponent } from './Journal/journal-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JournalHomeComponent
   ],
   imports: [
     BrowserModule
